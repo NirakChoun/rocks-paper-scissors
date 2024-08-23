0518e86910380d98570ca5206ff812ea0d187adc
NirakChoun <nirakchoun@gmail.com>
2024-08-22 21:04:02 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice === 0) {
@@ -15,13 +12,56 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = String(prompt("Enter your choice: ")).toLowerCase();
+    let humanChoice = String(prompt("Enter your choice: "));
     return humanChoice;
 }
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors");
+        }
+        else if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock");
+        }   
+        else { // computerChoice === "rock"
+            console.log("It's a draw!");
+        }
+    }
+    else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beats Paper");
+        }
+        else if (computerChoice === "paper") {
+            console.log("It's a draw!");
+        }   
+        else { // computerChoice === "rock"
+            console.log("You win! Paper beats Rock");
+        }
+    }
+    else { // humanChoice === "Scissors"
+        if (computerChoice === "scissors") {
+            console.log("It's a draw!");
+        }
+        else if (computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper");
+        }   
+        else { // computerChoice === "rock"
+            console.log("You lose! Rock beats Scissors");
+        }
+    }
+}
 
-console.log(`Computer Choice: ${computerChoice}`);
+let humanScore = 0;
+let computerScore = 0;
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
 console.log(`Human Choice: ${humanChoice}`);
+console.log(`Computer Choice: ${computerChoice}`);
+
+playRound(humanChoice, computerChoice);
+
 
