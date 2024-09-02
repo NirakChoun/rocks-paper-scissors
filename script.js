899c2fb35cc7c747a0f553566eb360d56a6c8555
NirakChoun <nirakchoun@gmail.com>
2024-09-01 21:37:30 -0500

    Add User Interface

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice === 0) {
-        return "rock";
-    }
-    else if (computerChoice === 1) {
-        return "paper";
-    }
-    else {
-        return "scissors";
-    }
-}
-
-function getHumanChoice() {
-    let humanChoice = String(prompt("Enter your choice: "));
-    return humanChoice;
-}
-
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    if (humanChoice === "rock") {
-        if (computerChoice === "scissors") {
+    if (humanChoice === "🪨") {
+        if (computerChoice === "✂️") {
             return "Player";
         }
-        else if (computerChoice === "paper") {
+        else if (computerChoice === "📝") {
             return "Computer";
         }   
         else { // computerChoice === "rock"
             return "Draw";
         }
     }
-    else if (humanChoice === "paper") {
-        if (computerChoice === "scissors") {
+    else if (humanChoice === "📝") {
+        if (computerChoice === "✂️") {
             return "Computer"
         }
-        else if (computerChoice === "paper") {
+        else if (computerChoice === "📝") {
             return "Draw";
         }   
         else { // computerChoice === "rock"
@@ -41,10 +22,10 @@ function playRound(humanChoice, computerChoice) {
         }
     }
     else { // humanChoice === "Scissors"
-        if (computerChoice === "scissors") {
+        if (computerChoice === "✂️") {
             return "Draw";
         }
-        else if (computerChoice === "paper") {
+        else if (computerChoice === "📝") {
             return "Player";
         }   
         else { // computerChoice === "rock"
@@ -65,49 +46,303 @@ function getWinner(humanScore, computerScore) {
     }
 }
 
-function playGame() {
-    let rounds = 1;
-    let humanScore = 0;
-    let computerScore = 0;
+// function playGame() {
+//     let rounds = 1;
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    while (rounds <= 5) {
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
+//     let playerScoreDisplay = document.querySelector("#playerScore");
+//     let computerScoreDisplay = document.querySelector("#computerScore");
+//     playerScoreDisplay.textContent = playerScore;
+//     computerScoreDisplay.textContent = playerScore;
 
-        console.log("\n");
-        console.log(`Human Choice: ${humanChoice}`);
-        console.log(`Computer Choice: ${computerChoice}`);
+//     let buttons = document.querySelector(".buttons");
 
-        let result = playRound(humanChoice, computerChoice);
-        if (result === "Player") {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        } 
-        else if (result === "Computer") {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-        else { // result === "Draw"
-            console.log("It's a draw!");
-        }
+//     while (rounds <= 5) {
+//         let playerChoice = document.querySelector("#playerChoice");
+//         let computerChoice = document.querySelector("#computerChoice");
 
-        console.log(`Human Score: ${humanScore}`);
-        console.log(`Computer Score: ${computerScore}`);
-        rounds++;
+//         buttons.addEventListener("click", (event) => {
+//             let target = event.target;
+//             let computerChoiceResult = getComputerChoice();
+//             let playerChoiceResult = "";
+//             switch(target.id) {
+//                 case "rockBtn":
+//                     playerChoice.textContent = "🪨";
+//                     playerChoiceResult = target.value;
+//                     computerChoice.textContent = computerChoiceResult;
+//                     break;
+//                 case "paperBtn":
+//                     playerChoice.textContent = "📝";
+//                     playerChoiceResult = target.value;
+//                     computerChoice.textContent = computerChoiceResult;
+//                     break;
+//                 case "scissorsBtn":
+//                     playerChoice.textContent = "✂️";
+//                     playerChoiceResult = target.value;
+//                     computerChoice.textContent = computerChoiceResult;
+//                     break;
+//             }
+            
+//         });
+
+//         console.log("\n");
+//         console.log(`Human Choice: ${humanChoice}`);
+//         console.log(`Computer Choice: ${computerChoice}`);
+
+//         let result = playRound(playerChoiceResult, computerChoiceResult);
+//         if (result === "Player") {
+//             alert(`You win! ${humanChoice} beats ${computerChoice}`);
+//             playerScore++;
+//             playerScoreDisplay.textContent = playerScore;
+//         } 
+//         else if (result === "Computer") {
+//             alert(`You lose! ${computerChoice} beats ${humanChoice}`);
+//             computerScore++;
+//             computerScoreDisplay.textContent = playerScore;
+//         }
+//         else { // result === "Draw"
+//             alert("It's a draw!");
+//         }
+
+//         console.log(`Human Score: ${playerScore}`);
+//         console.log(`Computer Score: ${computerScore}`);
+//         rounds++;
+//     }
+
+    // let winner = getWinner(playerScore, computerScore);
+    // if (winner === "Draw") {
+    //     alert(`It's a draw!`);
+    // }
+    // else {
+    //     alert(`The winner is ${winner}!`);
+    // }
+//     // playerScore = 0;
+//     // computerScore = 0;
+//     // playerScoreDisplay.textContent = playerScore;
+//     // computerScoreDisplay.textContent = playerScore;
+//     // console.log("\n");
+// }
+
+// playGame();
+
+
+// let buttons = document.querySelector(".buttons");
+
+// let playerChoice = document.querySelector("#playerChoice");
+// let computerChoice = document.querySelector("#computerChoice");
+
+// buttons.addEventListener("click", (event) => {
+//     let target = event.target;
+//     let playerChoiceResult = target.value;
+//     let computerChoiceResult = getComputerChoice();
+//     let result = playRound(playerChoiceResult, computerChoiceResult);
+//     switch(target.id) {
+//         case "rockBtn":
+//             playerChoice.textContent = "🪨";
+//             computerChoice.textContent = computerChoiceResult;
+//             if (result === "Player") {
+//                 setTimeout(() => {
+//                     alert(`You win! ${playerChoiceResult} beats ${computerChoiceResult}`);
+//                 }, 1000);
+//             } else if (result === "Computer") {
+//                 setTimeout(() => {
+//                     alert(`You lose! ${playerChoiceResult} beats ${computerChoiceResult}`);
+//                 }, 1000);
+//             } else {
+//                 setTimeout(() => {
+//                     alert("It's a draw!");
+//                 }, 1000);  
+//             } 
+//             break;
+//         case "paperBtn":
+//             playerChoice.textContent = "📝";
+//             computerChoice.textContent = computerChoiceResult;
+//             if (result === "Player") {
+//                 setTimeout(() => {
+//                     alert(`You win! ${playerChoiceResult} beats ${computerChoiceResult}`);
+//                 }, 1000);
+//             } else if (result === "Computer") {
+//                 setTimeout(() => {
+//                     alert(`You lose! ${playerChoiceResult} beats ${computerChoiceResult}`);
+//                 }, 1000);
+//             } else {
+//                 setTimeout(() => {
+//                     alert("It's a draw!");
+//                 }, 1000);  
+//             }
+//             break;
+//         case "scissorsBtn":
+//             playerChoice.textContent = "✂️";
+//             computerChoice.textContent = computerChoiceResult;
+//             if (result === "Player") {
+//                 setTimeout(() => {
+//                     alert(`You win! ${playerChoiceResult} beats ${computerChoiceResult}`);
+//                 }, 1000);
+//             } else if (result === "Computer") {
+//                 setTimeout(() => {
+//                     alert(`You lose! ${playerChoiceResult} beats ${computerChoiceResult}`);
+//                 }, 1000);
+//             } else {
+//                 setTimeout(() => {
+//                     alert("It's a draw!");
+//                 }, 1000);  
+//             }
+//             break;
+//     }
+// });
+
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3);
+    if (randomChoice === 0) {
+        return "🪨";
+    } else if (randomChoice === 1) {
+        return "📝";
     }
-
-    let winner = getWinner(humanScore, computerScore);
-    if (winner === "Draw") {
-        console.log(`It's a draw!`);
-    }
-    else {
-        console.log(`The winner is ${winner}!`);
-    }
-    
-    console.log("\n");
+    return "✂️";
 }
 
-playGame();
+// function playGame() {
+//     let rounds = 1;
+//     let playerScore = 0;
+//     let computerScore = 0;
+
+//     let playerScoreDisplay = document.querySelector("#playerScore");
+//     let computerScoreDisplay = document.querySelector("#computerScore");
+//     playerScoreDisplay.textContent = playerScore;
+//     computerScoreDisplay.textContent = playerScore;
+
+//     let buttons = document.querySelector(".buttons");
+
+//     let playerChoice = document.querySelector("#playerChoice");
+//     let computerChoice = document.querySelector("#computerChoice");
+
+//     while (rounds <= 5) {
+        
+//     }
+    
+// }
+
+let currentRound = 1;
+const maxRounds = 5;
+let playerScore = 0;
+let computerScore = 0;
+
+let playerScoreDisplay = document.querySelector("#playerScore");
+let computerScoreDisplay = document.querySelector("#computerScore");
+playerScoreDisplay.textContent = playerScore;
+computerScoreDisplay.textContent = playerScore;
+
+let buttons = document.querySelector(".buttons");
+
+let playerChoice = document.querySelector("#playerChoice");
+let computerChoice = document.querySelector("#computerChoice");
+
+buttons.addEventListener("click", (event) => {
+    let target = event.target;
+    let playerChoiceResult = target.value;
+    let computerChoiceResult = getComputerChoice();
+    let result = playRound(playerChoiceResult, computerChoiceResult);
+    switch(target.id) {
+        case "rockBtn":
+            playerChoice.textContent = "🪨";
+            computerChoice.textContent = computerChoiceResult;
+            if (result === "Player") {
+                playerScore++;
+                playerScoreDisplay.textContent = playerScore;
+                setTimeout(() => {
+                    alert(`You win! ${playerChoiceResult} beats ${computerChoiceResult}`);
+                }, 1000);
+                currentRound++;
+            } else if (result === "Computer") {
+                computerScore++;
+                computerScoreDisplay.textContent = computerScore;
+                setTimeout(() => {
+                    alert(`You lose! ${playerChoiceResult} beats ${computerChoiceResult}`);
+                }, 1000);
+                currentRound++;
+            } else {
+                playerScore++;
+                playerScoreDisplay.textContent = playerScore;
+                computerScore++;
+                computerScoreDisplay.textContent = computerScore;
+                setTimeout(() => {
+                    alert("It's a draw!");
+                }, 1000); 
+                currentRound++; 
+            } 
+            break;
+        case "paperBtn":
+            playerChoice.textContent = "📝";
+            computerChoice.textContent = computerChoiceResult;
+            if (result === "Player") {
+                playerScore++;
+                playerScoreDisplay.textContent = playerScore;
+                setTimeout(() => {
+                    alert(`You win! ${playerChoiceResult} beats ${computerChoiceResult}`);
+                }, 1000);
+                currentRound++;
+            } else if (result === "Computer") {
+                computerScore++;
+                computerScoreDisplay.textContent = computerScore;
+                setTimeout(() => {
+                    alert(`You lose! ${playerChoiceResult} beats ${computerChoiceResult}`);
+                }, 1000);
+                currentRound++;
+            } else {
+                playerScore++;
+                playerScoreDisplay.textContent = playerScore;
+                computerScore++;
+                computerScoreDisplay.textContent = computerScore;
+                setTimeout(() => {
+                    alert("It's a draw!");
+                }, 1000);  
+                currentRound++;
+            }
+            break;
+        case "scissorsBtn":
+            playerChoice.textContent = "✂️";
+            computerChoice.textContent = computerChoiceResult;
+            if (result === "Player") {
+                playerScore++;
+                playerScoreDisplay.textContent = playerScore;
+                setTimeout(() => {
+                    alert(`You win! ${playerChoiceResult} beats ${computerChoiceResult}`);
+                }, 1000);
+                currentRound++;
+            } else if (result === "Computer") {
+                computerScore++;
+                computerScoreDisplay.textContent = computerScore;
+                setTimeout(() => {
+                    alert(`You lose! ${playerChoiceResult} beats ${computerChoiceResult}`);
+                }, 1000);
+                currentRound++;
+            } else {
+                playerScore++;
+                playerScoreDisplay.textContent = playerScore;
+                computerScore++;
+                computerScoreDisplay.textContent = computerScore;
+                setTimeout(() => {
+                    alert("It's a draw!");
+                }, 1000); 
+                currentRound++; 
+            }
+            break;
+    }
+    if (currentRound > maxRounds) {
+        let winner = getWinner(playerScore, computerScore);
+        if (winner === "Draw") {
+            setTimeout(() => {
+                alert("Game Over! It's a draw!");
+            }, 2000);
+        }
+        else {
+            setTimeout(() => {
+                alert(`Game Over! The winner is ${winner}!`);
+            }, 2000);
+        }
+    }    
+});
 
 
 
@@ -115,3 +350,4 @@ playGame();
 
 
 
+// playGame();
